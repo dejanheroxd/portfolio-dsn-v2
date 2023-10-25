44df611d7345c96a72b5da8e9f35a51ca6ba1e7d
dejan <yugihohgx2@gmail.com>
2023-10-25 22:26:07 +0300

    Added world animation

--- src/pages/home/HeroSec/LocationWork.jsx
+++ src/pages/home/HeroSec/LocationWork.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Lottie from "lottie-react";
+import animationData from "../../../assets/Animation.json";
 
 export default function LocationWork() {
+  const worlRef = useRef();
+
+  useEffect(() => {
+    worlRef.current.setSpeed(0.55);
+  }, []);
+
   return (
     <div className="flex justify-between items-center left-0 right-0 h-36 absolute top-[39%]">
       <div className="bg-dennisDark w-[280px] items-center h-[110px] flex pl-10 rounded-r-full">
         <div className="text-white w-16 sm:text-xl">
-          <p className="leading-6">Located in the Greece</p>
+          <p className="leading-6">Located in Greece</p>
         </div>
-        <div className="ml-[82px] w-[77px] h-[77px] rounded-full  bg-dennisGray">
-          <img src="" alt="" />
+        <div className="ml-[82px] w-[77px] h-[77px] rounded-full bg-dennisGray">
+          <Lottie
+            initialSegment={[26, 114]}
+            lottieRef={worlRef}
+            loop={true}
+            animationData={animationData}
+            className="scale-[3]"
+          />
         </div>
       </div>
       <div className="relative mr-52 flex justify-center items-center">
@@ -21,8 +35,6 @@ export default function LocationWork() {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
               d="M0.406792 0.406792C0.949181 -0.135597 1.82859 -0.135597 2.37098 0.406792L22.2222 20.258V6.94444C22.2222 6.17739 22.8441 5.55555 23.6111 5.55555C24.3781 5.55555 25 6.17739 25 6.94444V23.6111C25 24.3781 24.3781 25 23.6111 25H6.94444C6.17739 25 5.55555 24.3781 5.55555 23.6111C5.55555 22.8441 6.17739 22.2222 6.94444 22.2222H20.258L0.406792 2.37098C-0.135597 1.82859 -0.135597 0.949181 0.406792 0.406792Z"
               fill="white"
             />
